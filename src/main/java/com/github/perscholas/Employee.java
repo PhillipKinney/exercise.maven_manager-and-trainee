e25115b8a4c15586022b27851386a72d4dbc149a
PhillipKinney <phillipjkinney@gmail.com>
2020-08-03 15:22:26 -0400

    update

---
+++ src/main/java/com/github/perscholas/Employee.java
@@ -0,0 +1,12 @@
+package com.github.perscholas;
+
+public class Employee {
+
+    long employeeID;
+    String employeeName;
+    String employeeAddress;
+    Long employeePhone;
+    double basicSalary;
+    double specialAllowance = 250.80;
+    double Hra = 1000.50;
+}
